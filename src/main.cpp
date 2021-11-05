14a5dc9a8588839b6e59cae6c2dfd232978f9236
xxxJotaxxx <joelruizlopez6@gmail.com>
2021-11-05 15:24:19 +0100

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,31 @@
+//libraries
 #include <Arduino.h>
 #include <IRremote.h>
 
+//pin receptor IR
 int ReceptorIR = 8;
 IRrecv irrecv(ReceptorIR);
 decode_results Codigo;
 
 void setup()
 {
+    //Initilize serial port
     Serial.begin(9600);
     Serial.print("...Initialize receptor...");
+    //Initialize IRremote receptor
     irrecv.enableIRIn();
 }
 
 void loop()
 {
+    //When receive a signal IR
     if (irrecv.decode(&Codigo))
     {
+        //Value in HEX
         Serial.print("0x");
         Serial.println(Codigo.value, HEX);
         delay(100);
+        //return the control to receiver
         irrecv.resume();
     }
 }
